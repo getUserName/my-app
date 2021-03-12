3ede3c61fd24c92eba5ead82a18a958d94ba65d0
mclozano <mclozano@courtesypoint.com>
2021-03-13 00:48:48 +0800

    .

--- src/js/gui/OrderForm.js
+++ src/js/gui/OrderForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
 import { InputNumber } from 'primereact/inputnumber';
@@ -7,45 +7,42 @@ import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import axios from 'axios';
 
-var data = [
-	{name: "User 1", category: "1"},
-	{name: "User 2", category: "1"},
-	{name: "User 3", category: "1"},
-	{name: "User 4", category: "2"},
-	{name: "User 5", category: "2"},
-	{name: "User 6", category: "2"},
-];
-
-const headerTemplate = (data) => {
-	return (
-		<React.Fragment>
-			<span>{data.category}</span>
-		</React.Fragment>
-	);
-}
-
-const footerTemplate = (data) => {
-	return (
-		<React.Fragment>
-		</React.Fragment>
-	);
-}
-
 var targetURI = 'https://handsonexam.herokuapp.com';
 var menuitemsURI = targetURI+'/menuitems';
 
 function OrderForm(){
 
+	let [menuitems, setMenuitems] = useState([])
+
 	useEffect(() => {
 		axios.get(menuitemsURI)
     	.then(res => {
+			res.data.sort((a,b)=>a.menuCategory.id-b.menuCategory.id);
         	console.log(res.data);
+			setMenuitems(res.data)
     	})
-	  });
+	  }, []);
+
+	const headerTemplate = (menuitems) => {
+		return (
+			<React.Fragment>
+				<span>{menuitems.menuCategory.name}</span>
+			</React.Fragment>
+		);
+	}
+	
+	const footerTemplate = () => {
+		return (
+			<React.Fragment>
+			</React.Fragment>
+		);
+	}
 
 	return (
-		<DataTable sortMode="multiple" value={data} rowGroupMode="subheader" groupField="category"
-		rowGroupHeaderTemplate={headerTemplate} rowGroupFooterTemplate={footerTemplate}>
+		<DataTable  value={menuitems}
+			rowGroupMode="subheader" groupField="menuCategory.name"
+			sortMode="single" sortField=".menuCategory.name" sortOrder={1}
+			rowGroupHeaderTemplate={headerTemplate} rowGroupFooterTemplate={footerTemplate}>
 			<Column field="name" header="Name"/>
 			<Column header="Quantity"
 
